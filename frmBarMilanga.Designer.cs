56dbf15032b8931227e6d386cfa9728350e8425a
delgadosofiaguada-jpg <delgado.sofiaguada@gmail.com>
2025-11-02 23:09:07 -0300

    interfaz terminada

--- frmBarMilanga.Designer.cs
+++ frmBarMilanga.Designer.cs
@@ -49,6 +49,10 @@
             textBox5 = new TextBox();
             textBox6 = new TextBox();
             textBox7 = new TextBox();
+            clmComidas = new DataGridViewTextBoxColumn();
+            clmPostres = new DataGridViewTextBoxColumn();
+            clmBebidasCONAlcohol = new DataGridViewTextBoxColumn();
+            clmBebidasSINAlcohol = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dgvResultados).BeginInit();
             SuspendLayout();
             // 
@@ -64,17 +68,19 @@
             // 
             // dgvResultados
             // 
+            dgvResultados.BackgroundColor = SystemColors.ButtonFace;
             dgvResultados.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvResultados.Location = new Point(12, 55);
+            dgvResultados.Columns.AddRange(new DataGridViewColumn[] { clmComidas, clmPostres, clmBebidasCONAlcohol, clmBebidasSINAlcohol });
+            dgvResultados.Location = new Point(8, 55);
             dgvResultados.Name = "dgvResultados";
             dgvResultados.RowHeadersWidth = 51;
-            dgvResultados.Size = new Size(739, 249);
+            dgvResultados.Size = new Size(598, 249);
             dgvResultados.TabIndex = 1;
             // 
             // cmdValidarDatos
             // 
-            cmdValidarDatos.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
-            cmdValidarDatos.Location = new Point(379, 325);
+            cmdValidarDatos.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cmdValidarDatos.Location = new Point(218, 328);
             cmdValidarDatos.Name = "cmdValidarDatos";
             cmdValidarDatos.Size = new Size(106, 29);
             cmdValidarDatos.TabIndex = 2;
@@ -84,8 +90,8 @@
             // 
             // cmdTotales
             // 
-            cmdTotales.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
-            cmdTotales.Location = new Point(645, 325);
+            cmdTotales.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cmdTotales.Location = new Point(500, 328);
             cmdTotales.Name = "cmdTotales";
             cmdTotales.Size = new Size(106, 29);
             cmdTotales.TabIndex = 3;
@@ -95,20 +101,20 @@
             // 
             // cmdMozoDia
             // 
-            cmdMozoDia.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
-            cmdMozoDia.Location = new Point(511, 325);
+            cmdMozoDia.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cmdMozoDia.Location = new Point(355, 328);
             cmdMozoDia.Name = "cmdMozoDia";
-            cmdMozoDia.Size = new Size(110, 29);
+            cmdMozoDia.Size = new Size(128, 29);
             cmdMozoDia.TabIndex = 4;
             cmdMozoDia.Text = "Mozo del Día";
             cmdMozoDia.UseVisualStyleBackColor = true;
             // 
             // cmdCerrar
             // 
-            cmdCerrar.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            cmdCerrar.Location = new Point(645, 595);
+            cmdCerrar.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cmdCerrar.Location = new Point(500, 586);
             cmdCerrar.Name = "cmdCerrar";
-            cmdCerrar.Size = new Size(106, 29);
+            cmdCerrar.Size = new Size(106, 38);
             cmdCerrar.TabIndex = 5;
             cmdCerrar.Text = "Cerrar";
             cmdCerrar.UseVisualStyleBackColor = true;
@@ -116,7 +122,7 @@
             // lblMozoDia
             // 
             lblMozoDia.AutoSize = true;
-            lblMozoDia.Location = new Point(420, 398);
+            lblMozoDia.Location = new Point(340, 397);
             lblMozoDia.Name = "lblMozoDia";
             lblMozoDia.Size = new Size(99, 20);
             lblMozoDia.TabIndex = 6;
@@ -125,7 +131,7 @@
             // lblTotBebSAlcohol
             // 
             lblTotBebSAlcohol.AutoSize = true;
-            lblTotBebSAlcohol.Location = new Point(25, 497);
+            lblTotBebSAlcohol.Location = new Point(8, 496);
             lblTotBebSAlcohol.Name = "lblTotBebSAlcohol";
             lblTotBebSAlcohol.Size = new Size(139, 20);
             lblTotBebSAlcohol.TabIndex = 7;
@@ -134,7 +140,7 @@
             // lblTotBebAlcohol
             // 
             lblTotBebAlcohol.AutoSize = true;
-            lblTotBebAlcohol.Location = new Point(25, 448);
+            lblTotBebAlcohol.Location = new Point(8, 447);
             lblTotBebAlcohol.Name = "lblTotBebAlcohol";
             lblTotBebAlcohol.Size = new Size(145, 20);
             lblTotBebAlcohol.TabIndex = 8;
@@ -143,7 +149,7 @@
             // lblTotComidas
             // 
             lblTotComidas.AutoSize = true;
-            lblTotComidas.Location = new Point(60, 398);
+            lblTotComidas.Location = new Point(43, 397);
             lblTotComidas.Name = "lblTotComidas";
             lblTotComidas.Size = new Size(104, 20);
             lblTotComidas.TabIndex = 9;
@@ -152,7 +158,7 @@
             // lblTotalG
             // 
             lblTotalG.AutoSize = true;
-            lblTotalG.Location = new Point(420, 509);
+            lblTotalG.Location = new Point(340, 508);
             lblTotalG.Name = "lblTotalG";
             lblTotalG.Size = new Size(97, 20);
             lblTotalG.TabIndex = 10;
@@ -161,7 +167,7 @@
             // lblImporte
             // 
             lblImporte.AutoSize = true;
-            lblImporte.Location = new Point(455, 458);
+            lblImporte.Location = new Point(375, 457);
             lblImporte.Name = "lblImporte";
             lblImporte.Size = new Size(62, 20);
             lblImporte.TabIndex = 11;
@@ -170,7 +176,7 @@
             // lblTotPostres
             // 
             lblTotPostres.AutoSize = true;
-            lblTotPostres.Location = new Point(109, 546);
+            lblTotPostres.Location = new Point(92, 545);
             lblTotPostres.Name = "lblTotPostres";
             lblTotPostres.Size = new Size(55, 20);
             lblTotPostres.TabIndex = 12;
@@ -178,58 +184,86 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(185, 391);
+            textBox1.Location = new Point(168, 390);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(125, 27);
             textBox1.TabIndex = 13;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(546, 451);
+            textBox2.Location = new Point(466, 450);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(125, 27);
             textBox2.TabIndex = 14;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(546, 391);
+            textBox3.Location = new Point(466, 390);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(125, 27);
             textBox3.TabIndex = 15;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(185, 539);
+            textBox4.Location = new Point(168, 538);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(125, 27);
             textBox4.TabIndex = 16;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(185, 490);
+            textBox5.Location = new Point(168, 489);
             textBox5.Name = "textBox5";
             textBox5.Size = new Size(125, 27);
             textBox5.TabIndex = 17;
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(185, 441);
+            textBox6.Location = new Point(168, 440);
             textBox6.Name = "textBox6";
             textBox6.Size = new Size(125, 27);
             textBox6.TabIndex = 18;
             // 
             // textBox7
             // 
-            textBox7.Location = new Point(546, 502);
+            textBox7.Location = new Point(466, 501);
             textBox7.Name = "textBox7";
             textBox7.Size = new Size(125, 27);
             textBox7.TabIndex = 19;
+            // 
+            // clmComidas
+            // 
+            clmComidas.HeaderText = "Comidas";
+            clmComidas.MinimumWidth = 6;
+            clmComidas.Name = "clmComidas";
+            clmComidas.Width = 125;
+            // 
+            // clmPostres
+            // 
+            clmPostres.HeaderText = "Postres";
+            clmPostres.MinimumWidth = 6;
+            clmPostres.Name = "clmPostres";
+            clmPostres.Width = 125;
+            // 
+            // clmBebidasCONAlcohol
+            // 
+            clmBebidasCONAlcohol.HeaderText = "Bebidas con alcohol";
+            clmBebidasCONAlcohol.MinimumWidth = 6;
+            clmBebidasCONAlcohol.Name = "clmBebidasCONAlcohol";
+            clmBebidasCONAlcohol.Width = 125;
+            // 
+            // clmBebidasSINAlcohol
+            // 
+            clmBebidasSINAlcohol.HeaderText = "Bebidas sin alcohol";
+            clmBebidasSINAlcohol.MinimumWidth = 6;
+            clmBebidasSINAlcohol.Name = "clmBebidasSINAlcohol";
+            clmBebidasSINAlcohol.Width = 125;
             // 
             // frmBarMilanga
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(775, 636);
+            ClientSize = new Size(633, 636);
             Controls.Add(textBox7);
             Controls.Add(textBox6);
             Controls.Add(textBox5);
@@ -280,5 +314,9 @@
         private TextBox textBox5;
         private TextBox textBox6;
         private TextBox textBox7;
+        private DataGridViewTextBoxColumn clmComidas;
+        private DataGridViewTextBoxColumn clmPostres;
+        private DataGridViewTextBoxColumn clmBebidasCONAlcohol;
+        private DataGridViewTextBoxColumn clmBebidasSINAlcohol;
     }
 }
