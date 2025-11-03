a2109e3d274a5f3249c0373dceac63ab20e15353
delgadosofiaguada-jpg <delgado.sofiaguada@gmail.com>
2025-11-02 23:23:57 -0300

    AvanceCodigo

--- frmBarMilanga.Designer.cs
+++ frmBarMilanga.Designer.cs
@@ -31,6 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBarMilanga));
             lblTitulo = new Label();
             dgvResultados = new DataGridView();
+            clmComidas = new DataGridViewTextBoxColumn();
+            clmPostres = new DataGridViewTextBoxColumn();
+            clmBebidasCONAlcohol = new DataGridViewTextBoxColumn();
+            clmBebidasSINAlcohol = new DataGridViewTextBoxColumn();
             cmdValidarDatos = new Button();
             cmdTotales = new Button();
             cmdMozoDia = new Button();
@@ -42,17 +46,13 @@
             lblTotalG = new Label();
             lblImporte = new Label();
             lblTotPostres = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
-            textBox6 = new TextBox();
-            textBox7 = new TextBox();
-            clmComidas = new DataGridViewTextBoxColumn();
-            clmPostres = new DataGridViewTextBoxColumn();
-            clmBebidasCONAlcohol = new DataGridViewTextBoxColumn();
-            clmBebidasSINAlcohol = new DataGridViewTextBoxColumn();
+            txtbTOTComidas = new TextBox();
+            txtbImporte = new TextBox();
+            txtbMozoDia = new TextBox();
+            txtbPostres = new TextBox();
+            txtbBebSAlcohol = new TextBox();
+            txtbBebAlcohol = new TextBox();
+            txtbTotGeneral = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dgvResultados).BeginInit();
             SuspendLayout();
             // 
@@ -77,6 +77,34 @@
             dgvResultados.Size = new Size(598, 249);
             dgvResultados.TabIndex = 1;
             // 
+            // clmComidas
+            // 
+            clmComidas.HeaderText = "Comidas";
+            clmComidas.MinimumWidth = 6;
+            clmComidas.Name = "clmComidas";
+            clmComidas.Width = 125;
+            // 
+            // clmPostres
+            // 
+            clmPostres.HeaderText = "Postres";
+            clmPostres.MinimumWidth = 6;
+            clmPostres.Name = "clmPostres";
+            clmPostres.Width = 125;
+            // 
+            // clmBebidasCONAlcohol
+            // 
+            clmBebidasCONAlcohol.HeaderText = "Bebidas con alcohol";
+            clmBebidasCONAlcohol.MinimumWidth = 6;
+            clmBebidasCONAlcohol.Name = "clmBebidasCONAlcohol";
+            clmBebidasCONAlcohol.Width = 125;
+            // 
+            // clmBebidasSINAlcohol
+            // 
+            clmBebidasSINAlcohol.HeaderText = "Bebidas sin alcohol";
+            clmBebidasSINAlcohol.MinimumWidth = 6;
+            clmBebidasSINAlcohol.Name = "clmBebidasSINAlcohol";
+            clmBebidasSINAlcohol.Width = 125;
+            // 
             // cmdValidarDatos
             // 
             cmdValidarDatos.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
@@ -97,7 +125,7 @@
             cmdTotales.TabIndex = 3;
             cmdTotales.Text = "Totales";
             cmdTotales.UseVisualStyleBackColor = true;
-            cmdTotales.Click += button1_Click;
+            cmdTotales.Click += cmdTotales_Click;
             // 
             // cmdMozoDia
             // 
@@ -108,6 +136,7 @@
             cmdMozoDia.TabIndex = 4;
             cmdMozoDia.Text = "Mozo del Día";
             cmdMozoDia.UseVisualStyleBackColor = true;
+            cmdMozoDia.Click += cmdMozoDia_Click;
             // 
             // cmdCerrar
             // 
@@ -118,6 +147,7 @@
             cmdCerrar.TabIndex = 5;
             cmdCerrar.Text = "Cerrar";
             cmdCerrar.UseVisualStyleBackColor = true;
+            cmdCerrar.Click += cmdCerrar_Click;
             // 
             // lblMozoDia
             // 
@@ -182,95 +212,67 @@
             lblTotPostres.TabIndex = 12;
             lblTotPostres.Text = "Postres";
             // 
-            // textBox1
+            // txtbTOTComidas
             // 
-            textBox1.Location = new Point(168, 390);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
-            textBox1.TabIndex = 13;
+            txtbTOTComidas.Location = new Point(168, 390);
+            txtbTOTComidas.Name = "txtbTOTComidas";
+            txtbTOTComidas.Size = new Size(125, 27);
+            txtbTOTComidas.TabIndex = 13;
             // 
-            // textBox2
+            // txtbImporte
             // 
-            textBox2.Location = new Point(466, 450);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
-            textBox2.TabIndex = 14;
+            txtbImporte.Location = new Point(466, 450);
+            txtbImporte.Name = "txtbImporte";
+            txtbImporte.Size = new Size(125, 27);
+            txtbImporte.TabIndex = 14;
             // 
-            // textBox3
+            // txtbMozoDia
             // 
-            textBox3.Location = new Point(466, 390);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(125, 27);
-            textBox3.TabIndex = 15;
+            txtbMozoDia.Location = new Point(466, 390);
+            txtbMozoDia.Name = "txtbMozoDia";
+            txtbMozoDia.Size = new Size(125, 27);
+            txtbMozoDia.TabIndex = 15;
             // 
-            // textBox4
+            // txtbPostres
             // 
-            textBox4.Location = new Point(168, 538);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(125, 27);
-            textBox4.TabIndex = 16;
+            txtbPostres.Location = new Point(168, 538);
+            txtbPostres.Name = "txtbPostres";
+            txtbPostres.Size = new Size(125, 27);
+            txtbPostres.TabIndex = 16;
             // 
-            // textBox5
+            // txtbBebSAlcohol
             // 
-            textBox5.Location = new Point(168, 489);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(125, 27);
-            textBox5.TabIndex = 17;
+            txtbBebSAlcohol.Location = new Point(168, 489);
+            txtbBebSAlcohol.Name = "txtbBebSAlcohol";
+            txtbBebSAlcohol.Size = new Size(125, 27);
+            txtbBebSAlcohol.TabIndex = 17;
             // 
-            // textBox6
+            // txtbBebAlcohol
             // 
-            textBox6.Location = new Point(168, 440);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(125, 27);
-            textBox6.TabIndex = 18;
+            txtbBebAlcohol.Location = new Point(168, 440);
+            txtbBebAlcohol.Name = "txtbBebAlcohol";
+            txtbBebAlcohol.Size = new Size(125, 27);
+            txtbBebAlcohol.TabIndex = 18;
             // 
-            // textBox7
+            // txtbTotGeneral
             // 
-            textBox7.Location = new Point(466, 501);
-            textBox7.Name = "textBox7";
-            textBox7.Size = new Size(125, 27);
-            textBox7.TabIndex = 19;
-            // 
-            // clmComidas
-            // 
-            clmComidas.HeaderText = "Comidas";
-            clmComidas.MinimumWidth = 6;
-            clmComidas.Name = "clmComidas";
-            clmComidas.Width = 125;
-            // 
-            // clmPostres
-            // 
-            clmPostres.HeaderText = "Postres";
-            clmPostres.MinimumWidth = 6;
-            clmPostres.Name = "clmPostres";
-            clmPostres.Width = 125;
-            // 
-            // clmBebidasCONAlcohol
-            // 
-            clmBebidasCONAlcohol.HeaderText = "Bebidas con alcohol";
-            clmBebidasCONAlcohol.MinimumWidth = 6;
-            clmBebidasCONAlcohol.Name = "clmBebidasCONAlcohol";
-            clmBebidasCONAlcohol.Width = 125;
-            // 
-            // clmBebidasSINAlcohol
-            // 
-            clmBebidasSINAlcohol.HeaderText = "Bebidas sin alcohol";
-            clmBebidasSINAlcohol.MinimumWidth = 6;
-            clmBebidasSINAlcohol.Name = "clmBebidasSINAlcohol";
-            clmBebidasSINAlcohol.Width = 125;
+            txtbTotGeneral.Location = new Point(466, 501);
+            txtbTotGeneral.Name = "txtbTotGeneral";
+            txtbTotGeneral.Size = new Size(125, 27);
+            txtbTotGeneral.TabIndex = 19;
             // 
             // frmBarMilanga
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(633, 636);
-            Controls.Add(textBox7);
-            Controls.Add(textBox6);
-            Controls.Add(textBox5);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            ClientSize = new Size(620, 636);
+            Controls.Add(txtbTotGeneral);
+            Controls.Add(txtbBebAlcohol);
+            Controls.Add(txtbBebSAlcohol);
+            Controls.Add(txtbPostres);
+            Controls.Add(txtbMozoDia);
+            Controls.Add(txtbImporte);
+            Controls.Add(txtbTOTComidas);
             Controls.Add(lblTotPostres);
             Controls.Add(lblImporte);
             Controls.Add(lblTotalG);
@@ -287,6 +289,7 @@
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "frmBarMilanga";
             Text = "Bar La Milanga";
+            Load += frmBarMilanga_Load;
             ((System.ComponentModel.ISupportInitialize)dgvResultados).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -307,13 +310,13 @@
         private Label lblTotalG;
         private Label lblImporte;
         private Label lblTotPostres;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
-        private TextBox textBox6;
-        private TextBox textBox7;
+        private TextBox txtbTOTComidas;
+        private TextBox txtbImporte;
+        private TextBox txtbMozoDia;
+        private TextBox txtbPostres;
+        private TextBox txtbBebSAlcohol;
+        private TextBox txtbBebAlcohol;
+        private TextBox txtbTotGeneral;
         private DataGridViewTextBoxColumn clmComidas;
         private DataGridViewTextBoxColumn clmPostres;
         private DataGridViewTextBoxColumn clmBebidasCONAlcohol;
--- frmBarMilanga.cs
+++ frmBarMilanga.cs
@@ -7,14 +7,118 @@ namespace pryDelgado_BarLaMilanga
             InitializeComponent();
         }
 
+        float[,] Ventas = new float[5, 4]; // 5 mozos x 4 categorías
+        string[] Mozos = { "Julio", "Esteban", "Javier", "Gonzalo", "Alberto" };
+        string[] Categorias = { "Comidas", "Bebidas sin alcohol", "Bebidas con alcohol", "Postres" };
+
+        private void frmBarMilanga_Load(object sender, EventArgs e)
+        {
+            dgvResultados.ColumnCount = 4;
+            dgvResultados.RowCount = 5;
+
+            dgvResultados.Columns[0].HeaderText = "Comidas";
+            dgvResultados.Columns[1].HeaderText = "Bebidas sin alcohol";
+            dgvResultados.Columns[2].HeaderText = "Bebidas con alcohol";
+            dgvResultados.Columns[3].HeaderText = "Postres";
+
+            string[] mozos = { "Julio", "Esteban", "Javier", "Gonzalo", "Alberto" };
+            for (int i = 0; i < 5; i++)
+            {
+                dgvResultados.Rows[i].HeaderCell.Value = mozos[i];
+            }
+            cmdMozoDia.Enabled = false;
+            cmdTotales.Enabled = false;
+        }
+
         private void cmdValidarDatos_Click(object sender, EventArgs e)
         {
+            bool valido = true;
 
+            for (int f = 0; f < 5; f++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    string valor = Convert.ToString(dgvResultados.Rows[f].Cells[c].Value);
+
+                    if (valor == "")
+                    {
+                        valido = false;
+                        break;
+                    }
+
+                    try
+                    {
+                        Ventas[f, c] = float.Parse(valor);
+                    }
+                    catch
+                    {
+                        valido = false;
+                        break;
+                    }
+                }
+            }
+            if (!valido)
+            {
+                MessageBox.Show("Error: Verifique que todas las celdas contengan valores numéricos válidos (puede usar 0).");
+                cmdMozoDia.Enabled = false;
+                cmdTotales.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Datos validados correctamente.");
+                cmdMozoDia.Enabled = true;
+                cmdTotales.Enabled = true;
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void cmdMozoDia_Click(object sender, EventArgs e)
         {
+            int mejorMozo = 0;
+            float mayorVenta = 0;
 
+            for (int f = 0; f < 5; f++)
+            {
+                float totalMozo = 0;
+                for (int c = 0; c < 4; c++)
+                {
+                    totalMozo += Ventas[f, c];
+                }
+
+                if (totalMozo > mayorVenta)
+                {
+                    mayorVenta = totalMozo;
+                    mejorMozo = f;
+                }
+            }
+
+            txtbMozoDia.Text = Mozos[mejorMozo];
+            txtbImporte.Text = mayorVenta.ToString("0.00");
+        }
+
+        private void cmdTotales_Click(object sender, EventArgs e)
+        {
+            float totalGeneral = 0;
+            float[] totalPorCategoria = new float[4];
+
+            for (int f = 0; f < 5; f++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    totalGeneral += Ventas[f, c];
+                    totalPorCategoria[c] += Ventas[f, c];
+                }
+            }
+
+            txtbTotGeneral.Text = totalGeneral.ToString("0.00");
+            txtbTOTComidas.Text = totalPorCategoria[0].ToString("0.00");
+            txtbBebSAlcohol.Text = totalPorCategoria[1].ToString("0.00");
+            txtbBebAlcohol.Text = totalPorCategoria[2].ToString("0.00");
+            txtbPostres.Text = totalPorCategoria[3].ToString("0.00");
+        }
+
+        private void cmdCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
-}
+} 
